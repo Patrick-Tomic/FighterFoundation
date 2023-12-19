122e3c2610b4003e0bbb135b5a55dda382317145
Patrick-Tomic <pat12tomic@gmail.com>
2023-12-19 10:53:36 -0500

    changing to typescript formatting, working on image being imported

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import './style.scss'
+const logo = './images/logo.jpg'
+const header: HTMLElement | any = document.querySelector('header')
+const img = document.createElement('img')
+img.src = logo
+img.alt = 'fighter foundation'
+const imgDiv = document.createElement('div')
+imgDiv.appendChild(img)
+const imgHead = document.createElement('h3')
+imgHead.innerHTML = 'Fight to cure Glioblastoma!'
+imgDiv.appendChild(imgHead)
+header.appendChild(imgDiv)
+const h1 = document.createElement('h1')
+h1.innerHTML = '"I Think there is Always Hope"<br> -Michelle B. DeJong'
+header.appendChild(h1)
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ 
+module.exports = {
+	mode: 'development',
+	entry: {bundle: path.resolve(__dirname,'src/index.ts')},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+	},
+	plugins:[
+		new HtmlWebpackPlugin({
+			title: 'Fighter Foundation',
+			filename: 'index.html',
+			template: 'src/template.html'
+		}),
+	],
+  
+	module:{
+		rules:[
+			{
+				test: /\.scss$/i,
+				use:[
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+              },
+		],
+	},
+	optimization:{
+		runtimeChunk: 'single'
+	},
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+      },
+	  output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname,'dist'),
+		clean:true,
+         
+        
+	},
+};
